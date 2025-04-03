aeff494bc3ab872d1c32745f01054d503f22f979
Théo Poizat <hello@zatteo.com>
2025-04-03 07:59:58 +0200

    fix: Wrong title

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from '@rsbuild/core'
 import { getRsbuildConfig } from 'rsbuild-config-cozy-app'
 
 const config = getRsbuildConfig({
-  title: 'Twake Chat'
+  title: 'Twake Mail'
 })
 
 export default defineConfig(config)
