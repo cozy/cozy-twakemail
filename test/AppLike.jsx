ea4cb3ba371576456fe60d44a15000b501c83871
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-02-12 10:59:43 +0100

    feat: Upgrade packages

--- test/AppLike.jsx
+++ test/AppLike.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
+import I18n from 'twake-i18n'
 
 import { CozyProvider, createMockClient } from 'cozy-client'
 import AlertProvider from 'cozy-ui/transpiled/react/providers/Alert'
 import { BreakpointsProvider } from 'cozy-ui/transpiled/react/providers/Breakpoints'
-import I18n from 'cozy-ui/transpiled/react/providers/I18n'
 
 import enLocale from '../src/locales/en.json'
 
